e23976b493ddf209b5828fdd4d6cceb8f2e3de6e
Mateus Hideki <hidekisimote@hotmail.com.br>
2025-04-21 20:55:57 -0300

    ajustando producer para kafka

--- src/main/java/com/scraping/tcc/esalq/controller/Controller.java
+++ src/main/java/com/scraping/tcc/esalq/controller/Controller.java
@@ -1,6 +1,9 @@
 package com.scraping.tcc.esalq.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +21,16 @@ public class Controller {
 	@Autowired
 	private KafkaProducer producer;
 	
+    @Value("${spring.kafka.topic.producer}")
+    private String producerTopic;
+	
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+
     @GetMapping("/scraping/{ticker}")
     public Acao hello(@PathVariable String ticker) throws Exception {
-    	System.out.println("teste -> ");
+    	logger.info("Iniciando scraping...");
     	Acao acao = scraper.recuperaValor(ticker);
-    	System.out.println("teste -> "+  acao.getTicker());
-    	producer.sendMessage(acao.getTicker(), "scraping-topico");
+    	producer.sendMessage(acao.getTicker(), producerTopic);
         return acao;
     }
     
